313dc6e29bebda9a1e92419bf9f4d1f18e6d8ebc
PREETAM <preetamvarun99@gmail.com>
2023-12-17 22:33:23 +0000

    finished lesson 2

--- L-03/App.js
+++ L-03/App.js
@@ -1,46 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const h1 = React.createElement(
   'h1',
-  { id: 'heading-id' },
+  { class: 'heading' },
   'Hello world from react'
 );
 
-/* This is the exact reason why we have JSX */
-
-const div = React.createElement('div', { id: 'parent' }, [
-  React.createElement(
-    'div',
-    { class: 'children' },
-    React.createElement('p', {}, 'Paragraph-1')
-  ),
-  React.createElement(
-    'div',
-    { class: 'children' },
-    React.createElement('p', {}, 'Paragraph-2')
-  ),
-  React.createElement(
-    'div',
-    { class: 'children' },
-    React.createElement('p', {}, 'Paragraph-3')
-  ),
-]);
+const jsxHeading = <h1 class='heading'>Hello World from react</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(div);
 
-/* Create this structure using core react 
-  
-  <div id = "parent">
-      <div class = "children">
-          <p>Paragraph - 1</p>
-      </div>
-      <div class = "children">
-          <p>Paragraph - 2</p>
-      </div>
-      <div class = "children">
-          <p>Paragraph - 3</p>
-      </div>
-  </div>
-  
-  
-  
-  */
+// Functional Component In React (Has to be PascalCase)
+const Heading = () => {
+  return <h1>Heading</h1>;
+};
+
+const badAPI = () => {
+  return new Promise((resolve, reject) => {
+    resolve('Malacious Data');
+  });
+};
+
+const virus = badAPI();
+
+let MalaciousData = '';
+
+virus.then((msg) => {
+  MalaciousData = msg;
+});
+
+// when we are directly injecting javascript directly into the jsx. Jsx won't directly trust the jscode and
+// Inject it. instead It sanitises the data and it cross checks everything so that we can prevent
+// cross site scripting attacks from happening.
+
+const Content = () => {
+  return (
+    <div>
+      {MalaciousData}
+      <Heading />
+      <p>Paragraph Element</p>
+      <span>Span Element</span>
+    </div>
+  );
+};
+
+// root.render(Heading());
+
+root.render(<Content />);
